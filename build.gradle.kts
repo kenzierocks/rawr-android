c13f5d14cb08709f32edaf61120fdb0abb2f5ca4
Kenzie Togami <ket1999@gmail.com>
2019-07-14 00:34:37 -0700

    Update Gradle plugins, version to 5.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import java.util.Properties
-
 buildscript {
     repositories {
         google()
@@ -7,9 +5,9 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.0-alpha02")
+        classpath("com.android.tools.build:gradle:3.6.0-alpha04")
         classpath(kotlin("gradle-plugin", version = "1.3.20"))
-        classpath("gradle.plugin.com.techshroom:incise-blue:0.3.13")
+        classpath("gradle.plugin.com.techshroom:incise-blue:0.3.14")
     }
 }
 
